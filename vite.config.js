50e78ae8f0ca9833e13da85c2d6393d971a5bf01
subhadeepkristen335-stack <subhadeepkristen335@gmail.com>
2026-05-29 19:09:26 +0530

    'update'

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        // This is often the default, but good to be explicit
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`,
+      },
+    },
+  },
 })
